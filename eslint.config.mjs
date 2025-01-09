7eca5831a3c8b110edbf929d6d994d3dfd160efd
Anton <vo1dseeker333@gmail.com>
2025-01-09 19:34:50 +0200

    pre PR

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node, // зберігаємо підтримку для Node.js
+        ...globals.browser, // додаємо підтримку для браузерних глобальних змінних
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
