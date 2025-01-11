217d14c8c7c0c8b0178388b63eaec776606ce659
Anton <vo1dseeker333@gmail.com>
2025-01-11 17:37:17 +0200

    test meybe

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,13 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.node } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node, // Для бекенду
+        ...globals.browser, // Додаємо середовище браузера
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
